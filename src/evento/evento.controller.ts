9a0e55537f47244feff4dedfadc1a0902f104ee0
pmuruaga <pmuruaga@isovera.com>
2025-03-16 19:09:46 -0300

    Agregado CRUD de eventos Partidos y Eventos

--- src/evento/evento.controller.ts
+++ src/evento/evento.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Body, Param } from '@nestjs/common';
+import { EventoService } from './evento.service';
+import { Evento } from '@prisma/client';
 
-@Controller('evento')
-export class EventoController {}
+@Controller('eventos')
+export class EventoController {
+  constructor(private readonly eventoService: EventoService) {}
+
+  @Post()
+  crearEvento(@Body() data: { nombre: string; tipoEventoId: string; fechaInicio: Date; fechaFin: Date }) {
+    return this.eventoService.crearEvento(data);
+  }
+
+  @Get()
+  obtenerEventos() {
+    return this.eventoService.obtenerEventos();
+  }
+
+  @Get(':id')
+  obtenerEventoPorId(@Param('id') id: string) {
+    return this.eventoService.obtenerEventoPorId(id);
+  }
+
+  @Put(':id')
+  actualizarEvento(@Param('id') id: string, @Body() data: Partial<Evento>) {
+    return this.eventoService.actualizarEvento(id, data);
+  }
+
+  @Delete(':id')
+  eliminarEvento(@Param('id') id: string) {
+    return this.eventoService.eliminarEvento(id);
+  }
+}
--- src/partido/partido.module.ts
+++ src/partido/partido.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PartidoController } from './partido.controller';
 import { PartidoService } from './partido.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [PartidoController],
   providers: [PartidoService]
 })
